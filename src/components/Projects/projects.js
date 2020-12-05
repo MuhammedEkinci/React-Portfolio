07996d6823b32583200909da27385dcbdd7417a1
Muhammed Ekinci <siraceddinekinci@gmail.com>
2020-12-04 23:31:21 -0500

    added first project to portfolio

---
+++ src/components/Projects/projects.js
@@ -0,0 +1,13 @@
+const data = [
+    {
+        id: 1,
+        image:"https://github.com/MuhammedEkinci/Html-Quiz/raw/master/images/Home-page.png" ,
+        alt:"HTML-Quiz hompage",
+        name:"HTML-Quiz",
+        text:"This project is a small quiz web application where you answer some random questions. There is a timer and if the answer is wrong you lose time. When the time is up or if you answered all the questions you can save your highschore.",
+        appUrl: "https://muhammedekinci.github.io/Html-Quiz/index.html",
+        githubUrl: "https://github.com/MuhammedEkinci/Html-Quiz"
+    },
+]
+
+export default data;
--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,14 +1,58 @@
-
 import React from 'react';
-import { Container, Row, Col, Image } from "react-bootstrap";
-
+import { Container, Row, Col, Image, Card, CardImg, CardDeck, Button } from "react-bootstrap";
+import data from "../components/Projects/projects";
+import "../styles/Portfolio.css";
 
 function Portfolio() {
 
     return (
-        <div>
-
-        </div>
+        <Container fluid >
+            <Row>
+                <CardDeck>
+                    {data.map((project, i) => {
+                        return (
+                            <Col md={6}>
+                                <div key={i} className="cards">
+                                    <Card>
+                                        <Card.Img className='cardImage' src={project.image} alt={project.alt}></Card.Img>
+                                        <div className="card-body">
+                                            <Card.Body>
+                                                <Card.Title className="cardHeader">{project.name}</Card.Title>
+                                                <Card.Text className="devText">{project.text}</Card.Text>
+                                                <Button className="btn app" onClick={(e) => {e.preventDefault(); window.location.href=`${project.appUrl}`}}>Deploy The App</Button>
+                                                <Button className="btn githubBtn" onClick={(e) => {e.preventDefault(); window.location.href=`${project.githubUrl}`}}>Github Repository</Button>
+                                            </Card.Body>
+                                        </div>
+                                    </Card>
+                                </div>
+                            </Col>
+                        );
+                    })}
+                </CardDeck>
+            
+                {/* <div className="cardDeck">
+                <Col size="lg-12">
+                    <CardDeck>
+                        {data.map((dev, i) => {
+                            return (
+                                <div key={i} className="cards">
+                                    <Card.Body>
+                                        <CardImg className='cardImage' src={dev.image} alt={dev.alt}></CardImg>
+                                        <div className="devCardStuff">
+                                        <Card.Title className="cardHeader">{dev.name}</Card.Title>
+                                        <Card.Text className="devText">{dev.text}</Card.Text>
+                                        <button className="btn app"     onClick={(e) => {e.preventDefault(); window.location.href=`${dev.appUrl}`}}>Deploy The App</button>
+                                        <button className="btn githubBtn"     onClick={(e) => {e.preventDefault(); window.location.href=`${dev.githubUrl}`}}>Github Repository</button>
+                                        </div>
+                                    </Card.Body>
+                                </div>
+                            )
+                        })}
+                    </CardDeck>
+                </Col>
+                </div> */}
+            </Row>
+        </Container>
     );
 };
 
