ea67ae29bc98935bb638bf7bffa8043cf2d28d9e
Muhammed Ekinci <siraceddinekinci@gmail.com>
2020-12-06 07:01:22 -0500

    fixed bugs

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -15,7 +15,7 @@ function Home() {
                     <h1 className="display-4">My Name Is Muhammed Ekinci</h1>
                     <h2 className="about-me">Welcome To My Portfolio </h2>
                     <br></br>
-                    <NavLink className="btn btn-outline-secondary btn-lg text-center" to="#about">My Portfolio!</NavLink>
+                    <NavLink className="btn btn-outline-secondary btn-lg text-center" to="#about">My Portfolio</NavLink>
             </div>
             <AboutMe />
             <Portfolio />
