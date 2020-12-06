f8b22fc63772087068f04533d922953ee3f69171
Muhammed Ekinci <siraceddinekinci@gmail.com>
2020-12-06 06:37:57 -0500

    fixed navbar

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -4,7 +4,7 @@ import "./style.css";
 export function NavBar() {
   return (
     <nav className="navbar navbar-expand-sm navbar-dark bg-dark sticky-top">
-        <a className="navbar-brand" href="#">Muhammed Ekinci</a>
+        <a className="navbar-brand" href="#home">Muhammed Ekinci</a>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon" />
         </button>
